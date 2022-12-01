6cac31939a2c92ee03c5b50c8b85106afcc0ee08
daanbusch <70745576+daanbusch@users.noreply.github.com>
2022-12-01 13:59:11 +0100

    reset

--- colourMix/colourMix/ViewController.swift
+++ colourMix/colourMix/ViewController.swift
@@ -3,54 +3,62 @@
 //  colourMix
 //
 //  Created by SD on 10/11/2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
+    
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
-    
+    @IBOutlet weak var reset: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateColor()
+        colorView.backgroundColor = .black
     }
 
 
-    
-    
     @IBAction func switchChanged(_ sender: UISwitch) {
         updateColor()
         }
     
-        func updateColor() {
-            var red: CGFloat = 0
-            var green: CGFloat = 0
-            var blue: CGFloat = 0
-            if redSwitch.isOn {
-                red = 1
-            }
-            if greenSwitch.isOn{
-                green = 1
-            }
-            if blueSwitch.isOn {
-                blue = 1
-            }
-            let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
-            colorView.backgroundColor = color
-        }
+    @IBAction func sliderChanged(_ sender: Any) {
+    updateColor()
+    }
     
-    @IBAction func redSlider(_ sender: Any) {
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn{
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
     }
-    @IBAction func greenSlider(_ sender: Any) {
-    }
-    @IBAction func blueController(_ sender: Any) {
+    @IBAction func reset(_ sender: Any) {
+        redSwitch .isOn = false
+        greenSwitch .isOn = false
+        blueSwitch .isOn = false
+        redSlider .value = 0
+        greenSlider .value = 0
+        blueSlider .value = 0
+        updateColor()
     }
 }
-
-
